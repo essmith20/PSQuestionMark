8b84f02b70abf22a3e635f3d499323c09093e630
Anie44 <77950843+Anie44@users.noreply.github.com>
2021-04-21 22:57:00 -0400

    Add files via upload

---
+++ bottomRightCircle.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class bottomLeftCircle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class bottomRightCircle extends walls
+{
+    /**
+     * Act - do whatever the bottomLeftCircle wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
