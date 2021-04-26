baa84cf668b9b0963584dff9299e1a4dd4e7390a
essmith20 <71906020+essmith20@users.noreply.github.com>
2021-04-26 19:22:13 -0400

    Add files via upload

---
+++ health_bar.java
@@ -0,0 +1,31 @@
+/**
+ * Write a description of class health_bar here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class health_bar  
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class health_bar
+     */
+    public health_bar()
+    {String health_bar = "111";
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
