51ad2879132171973b9d55b8ed81ca0dbcbbcf45
Adnaan-Thadathil <77748800+Adnaan-Thadathil@users.noreply.github.com>
2021-05-08 15:32:38 -0400

    Add files via upload

---
+++ upperLeftCircle.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class upperLeftCircle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class upperLeftCircle extends walls
+{
+    /**
+     * Act - do whatever the upperLeftCircle wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
